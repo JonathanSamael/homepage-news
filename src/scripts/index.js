d9fdf307e893e3254e72f43778a5484ad9c10e87
Jonathan Amaral <jonathansamael@live.com>
2023-01-05 17:52:20 -0300

    "✨ feat: functionality to click and show toggleMenu"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,12 +11,31 @@
 */
 
 const btnMenu = document.querySelector(".hamburguer");
-const linesMenu = document.querySelectorAll(".menu");
 const overlay = document.querySelector(".overlay")
+const itemsMenu = document.querySelector(".items-menu");
+const linesMenu = document.getElementsByClassName("menu");
+
+function toggleMenu() {
+    overlay.classList.toggle("show-overlay")
+    itemsMenu.classList.toggle("show-menu")
+}
+
+function menuClosed() {
+
+    if (linesMenu) {
+        linesMenu[0].classList.add("closed")
+        linesMenu[1].classList.add("closed")
+        linesMenu[2].classList.add("closed")
+    } else {
+        linesMenu[0].classList.remove("closed")
+        linesMenu[1].classList.remove("closed")
+        linesMenu[2].classList.remove("closed")
+    }
+
+}
 
 btnMenu.addEventListener("click", () => {
+    toggleMenu();
+    menuClosed();
+});
 
-    const itemsMenu = document.querySelector(".items-menu");
-
-    itemsMenu.classList.remove("show-menu")
-})
