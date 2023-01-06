ef372ada1969e8fba9157c30f31f1b02f895b80a
Jonathan Amaral <jonathansamael@live.com>
2023-01-06 12:15:56 -0300

    "🎯 fix: toggleMenu ins't work"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 const btnMenu = document.querySelector(".hamburguer");
 const overlay = document.querySelector(".overlay")
 const itemsMenu = document.querySelector(".items-menu");
-const linesMenu = document.getElementsByClassName("menu");
+const linesMenu = document.querySelectorAll(".menu");
 
 function toggleMenu() {
     overlay.classList.toggle("show-overlay")
@@ -10,17 +10,26 @@ function toggleMenu() {
 
 function menuClosed() {
 
-    if (linesMenu) {
-        linesMenu[0].classList.add("closed")
-        linesMenu[1].classList.add("closed")
-        linesMenu[2].classList.add("closed")
-    } else {
-        linesMenu[0].classList.remove("closed")
-        linesMenu[1].classList.remove("closed")
-        linesMenu[2].classList.remove("closed")
-    }
+    linesMenu.forEach(line => {
+    
+        if (line.classList == "menu") {
 
-}
+            linesMenu[0].classList.add("closed")
+            linesMenu[1].classList.add("closed")
+            linesMenu[2].classList.add("closed")
+
+        } else if (line.classList.contains("closed")){
+
+            linesMenu[0].classList.remove("closed")
+            linesMenu[1].classList.remove("closed")
+            linesMenu[2].classList.remove("closed")
+        }
+    })
+};
+
+
+
+
 
 btnMenu.addEventListener("click", () => {
     toggleMenu();
